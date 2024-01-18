00a54fdada0583a8405cf73d0c1057a08465a613
focusmode <focusmode@CAITLYNSFOCE86F>
2024-01-17 20:16:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using MoreCSharpFun;
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Constructor
+        PrintStuff ps = new PrintStuff();
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+
+}
